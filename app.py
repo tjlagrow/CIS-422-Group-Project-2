51fdc3b7f326e3961c689cf20e7a6e3819b51d14
J <my@email.com>
2017-05-05 08:49:45 +0000

    Frontend

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "secret_key"
+
+
+#main page 
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port = 8080, debug = True)
