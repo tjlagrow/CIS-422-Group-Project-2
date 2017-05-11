c802c2787373a5e714929de0d66ed1e1fb9424c9
Daniel S <dsu@uoregon.edu>
2017-05-11 11:43:19 -0700

    added mobile friendly view in main.html

---
+++ templates/image_uploading.js
@@ -0,0 +1,29 @@
+<script>
+  function previewFile(){
+    var preview = document.querySelector('img'); //selects the query named img
+    var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        preview.src = "";
+    }           
+  };
+
+  previewFile();  //calls the function named previewFile()
+
+ /* function isUploadSupported() {
+    if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+        return false;
+    }
+    var elem = document.createElement('input');
+    elem.type = 'file';
+    return !elem.disabled;
+  };*/
+
+  </script>
