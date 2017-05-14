3b6a646e3961361e1df07279741e06f4054b9371
Daniel S <dsu@uoregon.edu>
2017-05-14 16:29:56 -0700

    create static folders for images, update image path for main, & comment out upload in flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,12 +9,8 @@ app.secret_key = "secret_key"
 #main page 
 @app.route('/')
 def main():
-    return render_template('CIS-422-Group-Project-2/templates/main.html')
-
-@app.route('/dashboard',methods=('GET',))
-def dashboard():
-    return render_template('dashboard.html')
-	
+    return render_template('main.html')
+'''	
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
    if request.method == 'POST':
@@ -25,7 +21,7 @@ def upload():
       app.config['UPLOAD_FOLDER'] = 'static/Uploads'
       file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
       return json.dumps({'filename':f_name})
-
+'''
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port = 8080, debug = True)
