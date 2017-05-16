09cd6e9084cd6a81a66985d85b5d795f060803b1
Daniel S <dsu@uoregon.edu>
2017-05-15 20:48:15 -0700

    add upload image function on flask & submit form on main.html

--- flask_app.py
+++ flask_app.py
@@ -5,11 +5,44 @@ from werkzeug import secure_filename
 app = Flask(__name__)
 app.secret_key = "secret_key"
 
+app.config['input_images'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
-#main page 
+#main page
 @app.route('/')
 def main():
     return render_template('main.html')
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['input_images'], filename)
+
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
 
 #@app.route('/upload', methods = ['GET', 'POST'])
 #def upload():
