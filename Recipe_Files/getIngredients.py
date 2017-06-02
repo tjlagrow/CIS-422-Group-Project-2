23c0ec01ce4a27d2051bd5b992e032b63d96d486
Theodore J. LaGrow <tjlagrow@gmail.com>
2017-06-02 09:00:41 -0700

    Update update! Read all about it!

--- Recipe_Files/getIngredients.py
+++ Recipe_Files/getIngredients.py
@@ -1,7 +1,12 @@
 """
-filename: getIngredients.py
-author: Theodore J. LaGrow
-language: python 2.7x
+filename: 
+	getIngredients.py
+author: 
+	Theodore J. LaGrow
+language: 
+	python 2.7x
+use:
+	If given a string of words, should output a JSON of ingredients.
 """
 
 import unirest
@@ -10,7 +15,7 @@ import json
 
 ##############################################################
 
-# Need the API Key
+# A verification of the API in the correct folder
 if os.path.isfile("./SPOONACULAR_API_KEY.txt") != True:
 	exit("API Key does not exist.")
 else:
@@ -22,8 +27,10 @@ else:
 
 def getIngredientsFromString():
 
+	# stub string
 	string = "Hello, world! This is a test. I have a tomato in the fridge.  There is a bundle of carrots next to it with three potatos.  There is a jar of mayo next to it."
-
+	
+	# making Spoonacular API call
 	response = unirest.post("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/detect",
 		headers={
 		"X-Mashape-Key": SPOONACULAR_API_KEY,
@@ -35,10 +42,18 @@ def getIngredientsFromString():
 		}	
 	)
 
+	# grabbing the data produced by the API call
 	b = response.raw_body
+	# loading data into a json format
 	b_json = json.loads(b)
+	
+	ingredients = {}
+	ingredients["ingredients"] = []
+	for i in range(len(b_json["annotations"])):
+		ingredients["ingredients"].append(b_json["annotations"][i]["annotation"])
+	# putting the extracted data into a json file	
 	with open("JSON_Files/ingredientsFromString.json", "w") as f:
-		json.dump(b_json, f, indent=1)
+		json.dump(ingredients, f, indent=1)
 
 
 if __name__ == "__main__":
